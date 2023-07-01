5d1364bbf82b8c10b1823e730a2ca94d5664d336
BAGGARI <karim.baggari@capgemini.com>
2023-07-01 18:46:15 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+  return (
+    <Fragment>
+      <Header />
+      {!isAuth ? <Auth /> : <UserProfile />}
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
